f764accc823df5732d59b6b3d9a03bca7f159ab8
Laycon <layconjohn.sg@gmail.com>
2022-04-07 16:42:00 -0300

    Adicionar gifs em uma lista e criar o template

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,13 @@
 let numeroCartas;
+let gifsCard = [
+   `<img src="midias/bobrossparrot.gif" class="verso oculto"></img>`,
+   `<img src="midias/explodyparrot.gif" class="verso oculto">`,
+   `<img src="midias/fiestaparrot.gif" class="verso oculto">`,
+   `<img src="midias/metalparrot.gif" class="verso oculto">`,
+   `<img src="midias/revertitparrot.gif" class="verso oculto">`,
+   `<img src="midias/tripletsparrot.gif" class="verso oculto">`,
+   `<img src="midias/unicornparrot.gif" class="verso oculto">`
+]
 
 function inserirCartas() {
     // receber o número de cartas
@@ -8,12 +17,18 @@ function inserirCartas() {
         numeroCartas = prompt("Por favor, selecione apenas valores pares entre 4 e 14:")
      }
      let contador = 0;
+     let index = 0;
      let espacoCard = document.querySelector('.cards')
      //espacoCard.innerHTML += `<div class="card"><img src="midias/front.png" alt=""></div>`
      while (contador < numeroCartas) {
-        espacoCard.innerHTML += `<div class="card"><img src="midias/front.png" alt=""></div>`
+        espacoCard.innerHTML += `<div class="card"><img src="midias/front.png" class="frente">${gifsCard[index]}</div>`
         contador ++
      }
+     if (index < gifsCard.length) {
+      index ++
+   } else {
+      index = 1
+   }
 }
 
 function validarCartas(numeroCartas) {
