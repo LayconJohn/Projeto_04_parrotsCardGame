e7b16703b7c82bf102f2e069101898f82b0840b4
Laycon <layconjohn.sg@gmail.com>
2022-04-07 23:45:47 -0300

    Criação de lógica e esboço Para requisitos finais

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,12 +1,12 @@
 let numeroCartas;
 let gifsCard = [
-   `<img src="midias/bobrossparrot.gif" class="verso oculto">`,
-   `<img src="midias/explodyparrot.gif" class="verso oculto">`,
-   `<img src="midias/fiestaparrot.gif" class="verso oculto">`,
-   `<img src="midias/metalparrot.gif" class="verso oculto">`,
-   `<img src="midias/revertitparrot.gif" class="verso oculto">`,
-   `<img src="midias/tripletsparrot.gif" class="verso oculto">`,
-   `<img src="midias/unicornparrot.gif" class="verso oculto">`
+   `<img src="midias/bobrossparrot.gif" class="verso bobross oculto">`,
+   `<img src="midias/explodyparrot.gif" class="verso explody oculto">`,
+   `<img src="midias/fiestaparrot.gif" class="verso fiesta oculto">`,
+   `<img src="midias/metalparrot.gif" class="verso metal oculto">`,
+   `<img src="midias/revertitparrot.gif" class="verso revertit oculto">`,
+   `<img src="midias/tripletsparrot.gif" class="verso triplets oculto">`,
+   `<img src="midias/unicornparrot.gif" class="verso unicorn oculto">`
 ]
 
 function inserirCartas() {
@@ -40,7 +40,7 @@ function inserirCartas() {
   // }
 }
 
-function validarCartas(numeroCartas) {
+function validarParCartas(numeroCartas) {
     const ePar = (numeroCartas%2 == 0)
     return ePar   
 }
@@ -50,6 +50,40 @@ function clicarCarta (elemento) {
    let cardVerso = elemento.querySelector(".verso img")
    cardFrente.classList.add("oculto")
    cardVerso.classList.remove("oculto")
+}
+
+function verificarPeimeiraCarta () {
+   // Verificar e retornar se é a primeira carta virada da classe (true ou false)
+}
+
+function verificarPares() {
+   //Verificar e retornar se os pares são iguais (true ou false) -> Talvez usar o switch case
+}
+
+function contadorJogadas() {
+   // A cada jogada, contabilizar o número de cartas viradas
+}
+
+function virarCarta() {
+   //Virar a carta
+}
+
+function fimJogo() {
+   // Dar um alert sobre o total de jogadas
+   //incluir espaço para bônus
+}
+if (verificarPeimeiraCarta) {
+   // Criar a função, ela deve permanecer virada até verificar a segunda      
+   contadorJogadas()
+} else {
+   //Caso seja a segunda carta
+   contadorJogadas()
+   if (verificarPares) {
+      //Se as duas são pares, elas devem ficar viradas
+   } else {
+      // Se não são iguais, as duas cartas devem virar
+      virarCarta()
+   }
 }
 
 inserirCartas()
